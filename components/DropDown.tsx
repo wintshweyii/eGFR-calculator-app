6f43acaab2e62874a4b174e18716965c2ca04392
wintshweyii <wintshwey4i@gmail.com>
2026-02-24 14:20:59 +0630

    add DropDown component file

---
+++ components/DropDown.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Dropdown } from "react-native-element-dropdown";
+
+type DropdownItem = {
+  id: number | string;
+  name: string;
+};
+
+type Props = {
+  data: DropdownItem[];    
+  selectedValue: number | string | null;
+  onSelect: (id: number | string | null) => void;
+};
+
+const CustomDropdown = ({data, selectedValue, onSelect }: Props) => {
+  return (
+    <View style={styles.container}>
+      <Dropdown
+        style={styles.dropdown}
+        containerStyle={styles.dropdownContainer}
+        itemContainerStyle={styles.itemContainer}
+        itemTextStyle={styles.itemText}
+        selectedTextStyle={styles.selectedText}
+        activeColor="#d9ecfa"
+        data={data}
+        labelField="name"
+        valueField="id"
+        value={selectedValue}
+        onChange={(item) => onSelect(item.id)}
+      />
+    </View>
+  );
+};
+
+export default CustomDropdown;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  dropdown: {
+    marginLeft: 5,
+    paddingVertical: 8,
+    paddingHorizontal: 10,
+    backgroundColor: 'white',
+    borderRadius: 6,
+    width: 110,
+    height: 30
+  },
+
+  dropdownContainer: {
+    borderRadius: 10,
+  },
+  selectedText: {
+    color: '#000000',
+    fontSize: 11
+  },
+  itemContainer: {
+    height: 50
+  },
+  itemText: {
+    color: '#1691E9',
+    fontSize: 10,
+  },
+});
---
+++ components/delete-confirm-modal.tsx
@@ -0,0 +1,110 @@
+import Octicons from '@expo/vector-icons/Octicons';
+import React from "react";
+import {
+    KeyboardAvoidingView,
+    Modal,
+    Platform,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+} from "react-native";
+
+type DeleteGoalProps = {
+  visible: boolean;
+  onConfirm: () => void; 
+  onCancel: () => void;
+  confirmText: string
+};
+
+const ConfirmModal = ({ visible, onConfirm, onCancel, confirmText }: DeleteGoalProps) => {
+
+  return (
+    <Modal visible={visible} transparent animationType="fade" statusBarTranslucent>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <View style={styles.overlay}>
+          <View style={styles.container}>
+            
+            <View style={styles.titleContent}>
+              <Octicons name="alert" size={30} color="#b00020" />
+              <Text style={styles.titleText}>
+                {confirmText}
+              </Text>
+            </View>
+
+            <View style={styles.buttonContent}>
+              <TouchableOpacity
+                onPress={onConfirm}
+                style={[styles.button, styles.deleteBtn]}
+              >
+                <Text style={styles.deleteBtnText}>Delete</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                onPress={onCancel}
+                style={[styles.button, styles.cancelBtn]}
+              >
+                <Text style={styles.cancelText}>Cancel</Text>
+              </TouchableOpacity>
+            </View>
+
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </Modal>
+  );
+};
+
+export default ConfirmModal;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#ffffff',
+    width: '80%',
+    borderRadius: 20,
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.45)',
+    justifyContent: 'center',
+    alignItems: "center",
+  },
+  titleContent: {
+    alignItems: 'center',
+    marginBottom: 25,
+  },
+  titleText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    textAlign: 'center',
+    marginTop: 10,
+  },
+  buttonContent: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  button: {
+    borderRadius: 10,
+    padding: 12,
+    width: '45%',
+    alignItems: "center",
+  },
+  deleteBtn: {
+    backgroundColor: "#b00020",
+  },
+  deleteBtnText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  cancelBtn: {
+    backgroundColor: "#e0e0e0",
+  },
+  cancelText: {
+    fontWeight: "500",
+  },
+});
